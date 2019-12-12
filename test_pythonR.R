8ee3ea7e5af10f437df6e8c0a1537a2bd4e61234
Lorenza Pacini <lorenza.pacini@gmail.com>
2019-12-12 23:54:12 +0100

    update test

--- test_pythonR.R
+++ test_pythonR.R
@@ -10,11 +10,19 @@ py_run_file("buildings_network.py", convert = FALSE)
 # source_python("buildings_network.py", convert = FALSE)
 
 point_coords <- c(45.745591, 4.871167)
-distance <- 1000
-distance_threshold <- 30
-B <- py$buildings_from_point(point_coords, distance=distance)
-py$plot_buildings(B) # something wrong here because of B object type
 
-# py$test_plot()
+py$B = py$Building(point_coords=point_coords)
+
+py$B$download_buildings()
+py$B$plot_buildings()
+py$B$merge_and_convex()
+py$B$plot_merged_buildings()
+py$B$assign_nodes()
+py$B$plot_nodes()
+py$B$assign_edges_weights()
+py$B$plot_edges()
+py$B$assign_network()
+py$B$plot_net()
+
 
 
