9338a0dc76faf1687f4f3b96908651d3f4402ac8
Lorenza Pacini <lorenza.pacini@gmail.com>
2020-09-23 14:36:50 +0200

    update Building class

--- Building.py
+++ Building.py
@@ -3,7 +3,6 @@ import networkx as nx
 import geopandas as gpd
 import numpy as np
 import matplotlib as mpl
-# mpl.use('wxagg', force=True) 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import matplotlib.patches as patches
@@ -43,12 +42,13 @@ class Building():
             self.buildings = ox.project_gdf(self.buildings)
         self.is_downloaded = True
     
-    def plot_buildings(self, fc='black', ec='gray', figsize=(10, 10), imgs_folder = ".temp", filename="buildings", file_format='png', dpi=300):
+    def plot_buildings(self, fc='black', ec='gray', figsize=(10, 10), save=True, imgs_folder = ".temp", filename="buildings", file_format='png', dpi=300):
         if self.is_merged:
             raise Exception("merge_and_convex() already performed on Building. Please use plot_merged_buildings()")
         fig, ax = ox.plot_shape(self.buildings, fc=fc, ec=ec, figsize=figsize)
-        ox.settings.imgs_folder = imgs_folder
-        ox.save_and_show(fig, ax, save=True, show=False, close=True, filename=filename, file_format=file_format, dpi=dpi, axis_off=True)
+        if save:
+            ox.settings.imgs_folder = imgs_folder
+            ox.save_and_show(fig, ax, save=True, show=False, close=True, filename=filename, file_format=file_format, dpi=dpi, axis_off=True)
         plt.close()
     
     def merge_and_convex(self, buffer=0.01):
@@ -71,14 +71,17 @@ class Building():
         self.is_merged = True
         self.buildings_df = gpd.GeoDataFrame(geometry=[build for build in self.buildings])
 
-    def plot_merged_buildings(self, color='lightgray', edgecolor='black', figsize=(10, 10), imgs_folder = ".temp", filename="merged", file_format='png'):
+    def plot_merged_buildings(self, color='lightgray', edgecolor='black', figsize=(10, 10), save=True, imgs_folder = ".temp", filename="merged", file_format='png', show=True):
         if not self.is_merged:
             raise Exception("Please run merge_and_convex() before.")
         self.buildings.plot(figsize=figsize, color=color, edgecolor=edgecolor)
         plt.axis('off')
         plt.tight_layout()
-        os.makedirs(imgs_folder, exist_ok=True)
-        plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if save:
+            os.makedirs(imgs_folder, exist_ok=True)
+            plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
+            plt.show()
         plt.close()
 
     def assign_nodes(self):
@@ -108,24 +111,28 @@ class Building():
                             [node for node in self.nodes], columns=['color'])
         self.edges_assigned = True
     
-    def plot_nodes(self, figsize=(10, 10), colors=['lightgray', 'black'], markersize=0.1, imgs_folder = ".temp", filename="nodes", file_format='png'):
+    def plot_nodes(self, figsize=(10, 10), colors=['lightgray', 'black'], markersize=0.1, save=True, imgs_folder = ".temp", filename="nodes", file_format='png', show=True):
         cm = ListedColormap(colors, N=len(colors))
-        plt.figure()
         self.nodes_df.plot(figsize=figsize, column='color', markersize=markersize, cmap=cm)
         plt.axis('off')
         plt.tight_layout()
-        os.makedirs(imgs_folder, exist_ok=True)
-        plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if save:
+            os.makedirs(imgs_folder, exist_ok=True)
+            plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
+            plt.show()
         plt.close()
 
-    def plot_edges(self, figsize=(10, 10), colors=['lightgray', 'black'], markersize=1, linewidth=0.5, imgs_folder = ".temp", filename="edges", file_format='png'):
+    def plot_edges(self, figsize=(10, 10), colors=['lightgray', 'black'], markersize=1, linewidth=0.5, save=True, imgs_folder = ".temp", filename="edges", file_format='png', show=True):
         cm = ListedColormap(colors, N=len(colors))
-        plt.figure()
         self.edges_df.plot(figsize=figsize, column='color', markersize=markersize, linewidth=linewidth, cmap=cm)
         plt.axis('off')
         plt.tight_layout()
-        os.makedirs(imgs_folder, exist_ok=True)
-        plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if save:
+            os.makedirs(imgs_folder, exist_ok=True)
+            plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
+            plt.show()
         plt.close()
 
     def assign_network(self):
@@ -151,6 +158,7 @@ class Building():
         self.network_pos = pos
         self.net_assigned = True
         self.assign_node_color()
+        self.assign_edge_color()
 
     def assign_node_color(self, colors = ['blue', 'cyan', 'greenyellow', 'yellow', 'orange', 'red']):
         G = self.network
@@ -181,32 +189,57 @@ class Building():
                 node_color.append(colors[5])
         self.node_color = node_color
         self.neigh_watch_sharp_dict = neigh_watch_sharp_dict
-        self.colors = colors
+        self.colors_nodes = colors
 
-    def plot_net(self, figsize=(30, 30), imgs_folder = ".temp", filename="net", file_format='png'):
+    def assign_edge_color(self, colors = ['blue', 'cyan', 'greenyellow', 'yellow', 'orange', 'red']):
+        G = self.network
+        edge_color = []
+
+        for u, v in G.edges:
+            wij = G.get_edge_data(u, v)['weight']
+            if wij < 500: edge_color.append(colors[0])
+            elif wij < 1000:  edge_color.append(colors[1])
+            elif wij < 1500:  edge_color.append(colors[2])
+            elif wij < 2000:  edge_color.append(colors[3])
+            elif wij < 2500:  edge_color.append(colors[4])
+            else: edge_color.append(colors[5])
+        
+        self.edge_color = edge_color
+        self.colors_edges = colors
+
+
+    def plot_net(self, figsize=(30, 30), save=True, imgs_folder = ".temp", filename="net", file_format='png', show=True, style="node_color"):
         
         G = self.network
-        node_color = self.node_color
         
         weights_values = [self.weights[(u, v)] for u, v in G.edges]
         
         fig, ax  = plt.subplots(figsize=figsize)
         base = self.buildings_df.plot(ax=ax, color='gray', alpha=0.2)
         pos = self.network_pos
-        nx.draw_networkx_nodes(G, pos=pos, with_labels=False, node_color=node_color, edgecolors='k', ax=ax)
-        nx.draw_networkx_edges(G, pos=pos, width=[w * (2) ** (-8) for w in weights_values], ax=ax)
+        if style == "node_color":
+            node_color = self.node_color
+            nx.draw_networkx_nodes(G, pos=pos, with_labels=False, node_color=node_color, edgecolors='k', ax=ax)
+            nx.draw_networkx_edges(G, pos=pos, width=[w * (2) ** (-8) for w in weights_values], ax=ax)
+        elif style == "edge_color":
+            edge_color = self.edge_color
+            nx.draw_networkx_nodes(G, pos=pos, with_labels=False, edgecolors='k', node_color='w')
+            nx.draw_networkx_edges(G, pos=pos, width=[w * (2) ** (-8) for w in weights_values], edge_color=edge_color)
         plt.axis('off')
         plt.tight_layout()
-        os.makedirs(imgs_folder, exist_ok=True)
-        plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if save:
+            os.makedirs(imgs_folder, exist_ok=True)
+            plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
+            plt.show()
         plt.close() 
 
-    def plot_buildings_color(self, figsize=(30, 30), imgs_folder = ".temp", filename="buildings_color",
-        file_format='png'):
+    def plot_buildings_color(self, figsize=(30, 30), save=True, imgs_folder = ".temp", filename="buildings_color",
+        file_format='png', show=True):
             
             G = self.network
             node_color = self.node_color
-            colors = self.colors
+            colors = self.colors_nodes
             neigh_watch_sharp_dict = self.neigh_watch_sharp_dict
             buildings_df = self.buildings_df
 
@@ -225,5 +258,62 @@ class Building():
             ax.axis('off')
 
             plt.tight_layout()
+            if save:
+                plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+            if show:
+                plt.show()
+            plt.close()
+
+    def plot_nodes_legend(self, save=True, imgs_folder = ".temp", filename="legend_nodes" , file_format='png', show=True):
+        colors = self.colors_nodes
+        Glegend = nx.Graph()
+        for n in range(1, 7):
+            Glegend.add_node(str(n))
+
+        poslegend = {}
+        for i, node in enumerate(Glegend.nodes):
+            poslegend[node] = np.array([0, i + 0.15])
+
+        plt.figure() 
+        nx.draw_networkx_nodes(Glegend, poslegend, node_color=colors,edgecolors='k')
+        plt.axis('off')
+        plt.xlim(-0.2, 3)
+        plt.ylim(-0.2, 6)
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='calibri')
+        plt.text(0.2, 0, 'w/k $<$ 500 $m^2$',fontsize=16)
+        plt.text(0.2, 1, '500 ${m}^2$ $\leq$ w/k $<$ 1000 ${m}^2$',fontsize=16)
+        plt.text(0.2, 2, '1000 ${m}^2$ $\leq$ w/k $<$ 1500 ${m}^2$',fontsize=16)
+        plt.text(0.2, 3, '1500 ${m}^2$ $\leq$ w/k $<$ 2000 ${m}^2$',fontsize=16)
+        plt.text(0.2, 4, '2000 ${m}^2$ $\leq$ w/k $<$ 2500 ${m}^2$',fontsize=16)
+        plt.text(0.2, 5, 'w/k $>$ 2500 ${m}^2$',fontsize=16)
+        if save:
             plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
             plt.show()
+        plt.close()
+
+    def plot_edges_legend(self, save=True, imgs_folder = ".temp", filename="legend_edges" , file_format='png', show=True):
+        colors = self.colors_edges
+        texts = ['$w_{ij} \  <500\ m^2$',
+         '$500\ m^2 \leq \ w_{ij} \  < 1000\ m^2$',
+         '$1000\ m^2 \leq \ w_{ij} \  < 1500\ m^2$',
+         '$1500\ m^2 \leq \ w_{ij} \  < 2000\ m^2$',
+         '$2000\ m^2 \leq \ w_{ij} \  < 2500\ m^2$',
+         '$w_{ij} \  \geq 2500\ m^2$']
+        fig = plt.figure()
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='calibri')
+        for index, c in enumerate(colors):
+            subf = int('61%s' %(index + 1))
+            ax1 = fig.add_subplot(subf)
+            ax1.add_patch(patches.Rectangle((0.1, 0), 0.15, .4, facecolor=c, edgecolor='k',
+                                            linewidth=1.5))
+            ax1.axis('off')
+            ax1.text(0.3, 0, texts[index], fontsize=16)
+        plt.axis('off')
+        if save:
+            plt.savefig(os.path.join(imgs_folder, filename + "." + file_format))
+        if show:
+            plt.show()
+        plt.close()
